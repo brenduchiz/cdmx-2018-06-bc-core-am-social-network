f54084a9bf1791044788ccaaa288c4a79f256fe6
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-07-17 20:31:43 -0500

    Login

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let buttonRegistry = document.getElementById("registry");
+let buttonEnter = document.getElementById("enter");
+
+buttonRegistry.addEventListener("click", registryUser);
+buttonEnter.addEventListener("click", enterUser);
+
