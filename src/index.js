dd282d64abdcc5915dcacda6e5517e2e39951fb2
AngieMonroe <ame.monroy@gmail.com>
2018-07-23 13:14:28 -0500

    Agregando funcionalidad de los post

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-let buttonRegistry = document.getElementById("registry");
-let buttonEnter = document.getElementById("enter");
-let buttonGoogle = document.getElementById("loginGoogle");
-let buttonFacebook = document.getElementById("loginFace");
 
-buttonRegistry.addEventListener("click", registryUser);
-buttonEnter.addEventListener("click", enterUser);
-buttonGoogle.addEventListener("click", loginGoogle);
-buttonFacebook.addEventListener("click", loginFacebook);
-
+buttonRegistry.addEventListener('click', registryUser);
+buttonEnter.addEventListener('click', enterUser);
+buttonGoogle.addEventListener('click', loginGoogle);
+buttonSingOut.addEventListener('click', signOut);
+buttonPost.addEventListener('click', writeNewCommentary);
