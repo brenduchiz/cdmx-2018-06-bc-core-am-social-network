240634ee1c083446de0de1e3385b8118141fa45f
AngieMonroe <ame.monroy@gmail.com>
2018-07-24 18:15:30 -0500

    Actualizando readme y test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,1 +1,28 @@
-// Ejecuta tu test 
+describe('app', () => {
+  it('debería ser una función writterNewCommentary', () => {
+    assert.isFunction(writterNewCommentary);
+  });
+  describe('writterNewCommentary', () => {
+    it('debería retornar "Hola" ', () => {
+      assert.equal(writterNewCommentary(' '), 'Hola');
+    });
+  });
+
+  it('debería ser una función deleteComment', () => {
+    assert.isFunction(deleteComment);
+  });
+  describe('deleteComment', () => {
+    it('debería retornar null ', () => {
+      assert.equal(deleteComment('Hola'), null);
+    });
+  });
+
+  it('debería ser una función editComment en objeto global', () => {
+    assert.isFunction(editComment);
+  });
+  describe('editComment', () => {
+    it('debería retornar "Hola mundo"', () => {
+      assert.equal(editComment('Hola nundo'), 'Hola mundo');
+    });
+  });
+});
